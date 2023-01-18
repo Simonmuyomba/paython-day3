b05873364c62970bb4d8c83e38377d0ec0fa5f3a
PETER MUYOMBA <122818012+Simonmuyomba@users.noreply.github.com>
2023-01-18 15:21:31 +0000

    elif added

--- main.py
+++ main.py
@@ -5,5 +5,12 @@ user_input = int(input("What is your age? "))
 
 if user_input >= 16:
   print("Access granted ")
+  num = int(input(print("Enter your number : ")))
+  if num >= 100:
+    print("Congrats.....You may enter ")
+  elif num < 100:
+    print("Sorry...You're not allowed to enter ")
+  elif num == 0:
+    print("You not recognized ")
 else:
   print("Access denied!")
