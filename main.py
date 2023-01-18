174b1d02636b4ffe8acea5d901ad5d65e8deee60
PETER MUYOMBA <122818012+Simonmuyomba@users.noreply.github.com>
2023-01-18 14:50:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#Control flow with if / else and conditional operators
+
+print("Welcome to my page 🫥")
+user_input = int(input("What is your age? "))
+
+if user_input >= 16:
+  print("Access granted ")
+else:
+  print("Access denied!")
